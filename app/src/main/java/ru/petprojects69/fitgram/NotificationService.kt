a73e05c0593456a27085ee42dbf354f6b09b6b8f
NelsonBeard <ilnur111111@yandex.ru>
2022-08-23 08:57:29 +0500

    Исправления

--- app/src/main/java/ru/petprojects69/fitgram/NotificationService.kt
+++ app/src/main/java/ru/petprojects69/fitgram/NotificationService.kt
@@ -4,11 +4,6 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class NotificationService : FirebaseMessagingService() {
-
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
     }
-
-    override fun onNewToken(token: String) {
-    }
-
 }
