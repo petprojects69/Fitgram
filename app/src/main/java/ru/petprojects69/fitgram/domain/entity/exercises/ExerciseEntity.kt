7c1cacef477bd2ec4c594717f96241ee46518a72
NelsonBeard <ilnur111111@yandex.ru>
2022-08-26 17:38:57 +0500

    - Добавил 10 упражнений (дальше - больше) - Перенес предуставновленые упражнения в слой data

--- app/src/main/java/ru/petprojects69/fitgram/domain/entity/exercises/ExerciseEntity.kt
+++ app/src/main/java/ru/petprojects69/fitgram/domain/entity/exercises/ExerciseEntity.kt
@@ -5,10 +5,10 @@ import androidx.room.ColumnInfo
 data class ExerciseEntity(
     @ColumnInfo(name = "exercise_name") val name: String? = null,
     @ColumnInfo(name = "exercise_description") val description: String? = null,
-    @ColumnInfo(name = "exercise_poster") val poster: String? = null,
+    @ColumnInfo(name = "exercise_poster") val poster: Int? = null,
     @ColumnInfo(name = "exercise_difficulty") val difficulty: String? = null,
     @ColumnInfo(name = "exercise_location") val location: String? = null,
     @ColumnInfo(name = "exercise_burningCalories") val burningCalories: Int? = null,
     @ColumnInfo(name = "exercise_cooldown") val cooldown: Float? = null,
-    @ColumnInfo(name = "exercise_muscleGroup") val muscleGroup: Float? = null,
+    @ColumnInfo(name = "exercise_muscleGroup") val muscleGroup: String? = null,
 )
